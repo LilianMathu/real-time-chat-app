a1ac7f5bef75fa52b07baf13035d17fe75e4517a
LilianMathu <lilianmathu2018@gmail.com>
2019-08-06 16:23:51 +0300

    change port

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ let users = [];   //stores the users that have joined the conversation
 app.use(express.static(path.join(__dirname, "public")));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-
+app.get('/join', (req, res)=>{
+  console.log(success);
+})
 io.on('connection', function(socket) {
   console.log('new connection made');
 
